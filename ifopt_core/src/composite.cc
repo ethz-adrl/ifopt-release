5a44a52788f4ffc8536c42b8228af8a92e739eaf
Alexander Winkler <winklera@ethz.ch>
2018-07-30 14:16:30 +0200

    Rebase from 'release/melodic/ifopt'

--- ifopt_core/src/composite.cc
+++ ifopt_core/src/composite.cc
@@ -1,4 +1,4 @@
-/******************************************************************************
+﻿/******************************************************************************
 Copyright (c) 2017, Alexander W Winkler. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -56,6 +56,43 @@ std::string
 Component::GetName () const
 {
   return name_;
+}
+
+void Component::Print (double tol, int& index) const
+{
+  // calculate squared bound violation
+  VectorXd x = GetValues();
+  VecBound bounds = GetBounds();
+
+  std::vector<int> viol_idx;
+  for (uint i=0; i<bounds.size(); ++i) {
+    double lower = bounds.at(i).lower_;
+    double upper = bounds.at(i).upper_;
+    double val = x(i);
+    if (val < lower-tol || upper+tol < val)
+      viol_idx.push_back(i); // constraint out of bounds
+  }
+
+  std::string black = "\033[0m";
+  std::string red   = "\033[31m";
+  std::string color = viol_idx.empty()? black : red;
+
+  std::cout.precision(2);
+  std::cout << std::fixed
+            << std::left
+            << std::setw(30) << name_
+            << std::right
+            << std::setw(4) << num_rows_
+            << std::setw(9) << index
+            << std::setfill ('.')
+            << std::setw(7) << index+num_rows_-1
+            << std::setfill (' ')
+            << color
+            << std::setw(12) << viol_idx.size()
+            << black
+            << std::endl;
+
+  index += num_rows_;
 }
 
 Composite::Composite (const std::string& name, bool is_cost) :Component(0, name)
@@ -130,16 +167,21 @@ Composite::GetJacobian () const
   Jacobian jacobian(GetRows(), n_var);
 
   int row = 0;
+  std::vector< Eigen::Triplet<double> > triplet_list;
+
   for (const auto& c : components_) {
     const Jacobian& jac = c->GetJacobian();
+    triplet_list.reserve(triplet_list.size()+jac.nonZeros());
+
     for (int k=0; k<jac.outerSize(); ++k)
       for (Jacobian::InnerIterator it(jac,k); it; ++it)
-        jacobian.coeffRef(row+it.row(), it.col()) += it.value();
+        triplet_list.push_back(Eigen::Triplet<double>(row+it.row(), it.col(), it.value()));
 
     if (!is_cost_)
       row += c->GetRows();
   }
 
+  jacobian.setFromTriplets(triplet_list.begin(), triplet_list.end());
   return jacobian;
 }
 
@@ -161,56 +203,18 @@ Composite::GetComponents () const
   return components_;
 }
 
-// some printouts for convenience
-static int print_counter = 0;
 void
-Composite::Print (double tolerance) const
+Composite::PrintAll () const
 {
-  print_counter = 0;
+  int index = 0;
+  double tol = 0.001; ///< tolerance when printing out constraint/bound violation.
 
   std::cout << GetName() << ":\n";
   for (auto c : components_) {
     std::cout << "   "; // indent components
-    c->Print(tolerance);
+    c->Print(tol, index);
   }
   std::cout << std::endl;
-}
-
-void Component::Print (double tol) const
-{
-  // calculate squared bound violation
-  VectorXd x = GetValues();
-  VecBound bounds = GetBounds();
-
-  std::vector<int> viol_idx;
-  for (uint i=0; i<bounds.size(); ++i) {
-    double lower = bounds.at(i).lower_;
-    double upper = bounds.at(i).upper_;
-    double val = x(i);
-    if (val < lower-tol || upper+tol < val)
-      viol_idx.push_back(i); // constraint out of bounds
-  }
-
-  std::string black = "\033[0m";
-  std::string red   = "\033[31m";
-  std::string color = viol_idx.empty()? black : red;
-
-  std::cout.precision(2);
-  std::cout << std::fixed
-            << std::left
-            << std::setw(30) << name_
-            << std::right
-            << std::setw(4) << num_rows_
-            << std::setw(7) << print_counter
-            << std::setfill ('.')
-            << std::setw(7) << print_counter+num_rows_-1
-            << std::setfill (' ')
-            << color
-            << std::setw(6) << viol_idx.size()
-            << black
-            << std::endl;
-
-  print_counter += num_rows_;
 }
 
 } /* namespace opt */
